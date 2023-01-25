02bc1d3aff7aebfe50c08ec60e8f4123802e2f77
Milos <milos@screensailor.net>
2023-01-25 13:26:02 +0000

    park

--- App.tsx
+++ App.tsx
@@ -1,39 +1,61 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { interpolate } from "react-native-reanimated";
 import { AnimatedScrollViewWithContext } from "./src/AnimatedScrollViewWithContext";
 
-const data = Array(100)
-  .fill(null)
-  .map((_, i) => {
-    return {
-      value: Math.round(interpolate(Math.random(), [0, 1], [24, 84])),
-      id: i,
-    };
-  });
+const data = (count: number = 100) =>
+  Array(count)
+    .fill(null)
+    .map((_, i) => {
+      return {
+        value: Math.round(interpolate(Math.random(), [0, 1], [24, 84])),
+        id: i,
+      };
+    });
 
-const Tile = (props: Text["props"] & typeof data[number]) => (
+const Tile = (props: Text["props"] & ReturnType<typeof data>[number]) => (
   <Text style={[style.tile, { fontSize: props.value }]}>{props.value}</Text>
 );
 
+const Section = (props: { count: number }) => {
+  const color = `hsl(${Math.random() * 360}, 75%, 50%)`;
+  return (
+    <View
+      style={{
+        ...style.section,
+        backgroundColor: color,
+      }}
+    >
+      {data(props.count).map((item) => (
+        <Tile key={item.id} {...item} />
+      ))}
+    </View>
+  );
+};
+
 export default function App() {
   return (
-    <SafeAreaView>
-      <AnimatedScrollViewWithContext style={style.scrollView}>
-        {data.map((item) => (
-          <Tile key={item.id} {...item} />
-        ))}
+    <SafeAreaView style={style.safeArea}>
+      <AnimatedScrollViewWithContext>
+        <Section count={5} />
+        <Section count={5} />
+        <Section count={5} />
+        <Section count={5} />
+        <Section count={5} />
+        <Section count={5} />
       </AnimatedScrollViewWithContext>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
-  scrollView: {
-    padding: 20,
+  safeArea: {
+    backgroundColor: "dodgerblue",
+  },
+  section: {
+    paddingHorizontal: 20,
   },
   tile: {
-    backgroundColor: "dodgerblue",
-    color: "white",
+    backgroundColor: "white",
     marginTop: 20,
     textAlign: "center",
     width: "100%",
